0a4d2aa6ddeb7ef9a1b32e64a49efa993a8ad4a2
Keith Hoffmann <hi@keith.work>
2021-01-04 21:36:40 -0600

    feat: add placeholder pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About GTC" />
+  </Layout>
+)
+
+export default AboutPage
