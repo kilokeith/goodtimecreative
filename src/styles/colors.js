a764085ebb7ebfa22d7559f17633791abfd31305
Keith Hoffmann <hi@keith.work>
2021-01-05 21:01:13 -0600

    feat: add theme base configs

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+const baseRed = '#e7292a';
+
+const colors = {
+  red: baseRed,
+
+  lightGray: '#D1D1D1',
+  darkGray: '#353535',
+
+  white: '#ffffff',
+  black: '#000000',
+};
+
+export default colors;
