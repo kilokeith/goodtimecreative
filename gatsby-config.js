026c502e7fe10167f79fa8e6364111a1241b7cc1
Keith Hoffmann <hi@keith.work>
2021-01-12 17:53:32 -0600

    feat: add dotenv and func proxy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const env = process.env.NODE_ENV || 'development';
+require('dotenv').config({
+  path: `.env.${env}`,
+});
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = {
   siteMetadata: {
     title: `Good Time Creative`,
@@ -52,15 +58,15 @@ module.exports = {
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  // developMiddleware: app => {
-  //   app.use(
-  //     '/.netlify/functions/',
-  //     proxy({
-  //       target: 'http://localhost:9000',
-  //       pathRewrite: {
-  //         '/.netlify/functions/': '',
-  //       },
-  //     })
-  //   );
-  // },
+  developMiddleware: app => {
+    app.use(
+      '/.netlify/functions/',
+      createProxyMiddleware({
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          '/.netlify/functions/': '',
+        },
+      })
+    );
+  },
 };
