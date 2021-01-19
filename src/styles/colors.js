174201f19c76984e1ba10241b91167fcc02483de
Keith Hoffmann <hi@keith.work>
2021-01-19 13:31:19 -0600

    style: :art: change colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,8 +3,8 @@ const baseRed = '#e7292a';
 const baseColors = {
   red: baseRed,
   lightGray: '#D1D1D1',
-  darkGray: '#353535',
-  darkestGray: '#121111',
+  gray: '#353535',
+  darkGray: '#121111',
   white: '#ffffff',
   black: '#000000',
 };
