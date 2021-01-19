2b87d73eafeb2cc412f2f77ff2b9157d204a50fb
Keith Hoffmann <hi@keith.work>
2021-01-18 18:09:40 -0600

    feat: use site metadata hook

--- src/components/ui/layout.js
+++ src/components/ui/layout.js
@@ -8,7 +8,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // import styled from 'styled-components';
-import { useStaticQuery, graphql } from 'gatsby';
 import { ThemeProvider } from 'styled-components';
 import { ScreenClassProvider } from 'react-grid-system';
 import Header from '../header';
@@ -18,29 +17,25 @@ import FullWidthSection from './full-wdth-section';
 import theme from '../../styles/theme';
 // include global css with normalize.css reset
 import { GlobalStyle } from '../../styles/global.css.js';
+import { useSiteMetadata } from '../../hooks/use-site-metadata';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
+  const siteMetadata = useSiteMetadata();
 
   return (
     <ThemeProvider theme={theme}>
       <ScreenClassProvider>
         <GlobalStyle />
+
         <div id="page-outter-wrapper">
           <Container fluid>
-            <Header siteTitle={data.site.siteMetadata?.title || `GTC Home`} />
+            <Header siteTitle={siteMetadata?.title || `GTC Home`} />
           </Container>
+
           <Container>
             <main>{children}</main>
           </Container>
+
           <FullWidthSection style={{ backgroundColor: 'black' }}>
             <Container>
               <Footer />
