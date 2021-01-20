bfba8b6a3659901cdefbf7f3ffa1ab78569b07e9
Keith Hoffmann <hi@keith.work>
2021-01-19 21:48:48 -0600

    refactor: remove unused props

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
-import { Row, Col, Visible, Hidden } from 'react-grid-system';
-import { up, between } from 'styled-breakpoints';
-// import { theme } from 'styled-tools';
 import NavBar from './ui/navbar';
 
-const StyledHeader = styled.header`
-  padding: 2rem 2rem 2rem 2rem;
-`;
-
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <StyledHeader>
     <NavBar fixed={false} />
   </StyledHeader>
 );
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
+const StyledHeader = styled.header`
+  padding: 2rem 2rem 2rem 2rem;
+`;
 
-Header.defaultProps = {
-  siteTitle: ``,
-};
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// };
+
+// Header.defaultProps = {
+//   siteTitle: ``,
+// };
 
 export default Header;
