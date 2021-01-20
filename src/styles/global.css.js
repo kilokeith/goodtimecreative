d8418c94a89ecc4dae55b4eeb0c3553e0517572c
Keith Hoffmann <hi@keith.work>
2021-01-19 21:48:08 -0600

    style: :art: add class for background textures

--- src/styles/global.css.js
+++ src/styles/global.css.js
@@ -1,6 +1,8 @@
 import { css, createGlobalStyle } from 'styled-components';
 import typography from './typography.css';
-// import colors from './colors';
+import colors from './colors';
+import pinkGrit from '../images/backgrounds/pink-grit-bg.jpg';
+import blackGrit from '../images/backgrounds/black-grit-bg.jpg';
 
 // --- UPDATES TO GLOBAL STYLES REQUIRES A PAGE REFRESH ---
 
@@ -9,10 +11,23 @@ export const normalize = css`
   ${typography}
   body {
     max-width: 100vw;
+    min-height: 100vh;
     overflow-x: hidden;
   }
 `;
 // create a global page style
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
+
+  .pink-grit-bg {
+    background-image: url(${pinkGrit});
+    background-color: ${colors.white};
+    background-repeat: repeat;
+  }
+
+  .black-grit-bg {
+    background-image: url(${blackGrit});
+    background-color: ${colors.black};
+    background-repeat: repeat;
+  }
 `;
