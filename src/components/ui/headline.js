fb97946349bb968c4533d763f479379b19dec829
Keith Hoffmann <hi@keith.work>
2021-01-26 19:40:30 -0600

    feat: add left center right props to headline

--- src/components/ui/headline.js
+++ src/components/ui/headline.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
-import { switchProp } from 'styled-tools';
+import { switchProp, ifProp } from 'styled-tools';
 import { colorStyle } from '@styles/mixins/colors';
 
 const Headline = ({ children, size, color, ...props }) => (
@@ -13,6 +13,24 @@ const Headline = ({ children, size, color, ...props }) => (
 const StyledHTag = styled.h1`
   ${colorStyle};
   text-transform: uppercase;
+  ${ifProp(
+    'left',
+    css`
+      text-align: left;
+    `
+  )}
+  ${ifProp(
+    'center',
+    css`
+      text-align: center;
+    `
+  )}
+  ${ifProp(
+    'right',
+    css`
+      text-align: right;
+    `
+  )}
 
   ${switchProp('size', {
     h1: css`
@@ -34,11 +52,17 @@ Headline.propTypes = {
   children: PropTypes.node.isRequired,
   size: PropTypes.string,
   color: PropTypes.string,
+  left: PropTypes.bool,
+  center: PropTypes.bool,
+  right: PropTypes.bool,
 };
 
 Headline.defaultProps = {
   size: 'h1',
   color: 'white',
+  left: false,
+  center: false,
+  right: false,
 };
 
 export default Headline;
