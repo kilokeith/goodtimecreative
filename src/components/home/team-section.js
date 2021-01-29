a5228988874a7c2bd58fe50fd68bb25a1174a36b
Keith Hoffmann <hi@keith.work>
2021-01-29 16:35:09 -0600

    feat: flesh out team section with team images

--- src/components/home/team-section.js
+++ src/components/home/team-section.js
@@ -2,49 +2,57 @@ import React from 'react';
 import styled from 'styled-components';
 import { theme } from 'styled-tools';
 import { Row, Col } from 'react-grid-system';
-import { graphql, useStaticQuery } from 'gatsby';
 
 import BackgroundImage from 'gatsby-background-image';
-import Logo from '../logos/logo-round-face';
+import { useBackground } from '@hooks/use-background';
+import Container from '../ui/container';
+import Headline from '../ui/headline';
+import TeamList from './team-list';
 
 const TeamSection = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      bgImage: file(relativePath: { eq: "backgrounds/welder.jpg" }) {
-        childImageSharp {
-          fluid(quality: 90, maxWidth: 1428) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
-      }
-    }
-  `);
-
-  const imageData = data.bgImage.childImageSharp.fluid;
+  const background = useBackground('pink-grit-bg.jpg');
+  console.log('bg', background);
 
   return (
+    <StyledSection
+      Tag={'section'}
+      fluid={background.node.childImageSharp.fluid}
       id="team"
-        <CenteredCol xs={6} md={3}>
-          <StyledLogo />
-        </CenteredCol>
-      </Row>
+    >
+      <Container>
+        <Row align="center" justify="center" nogutter nowrap>
+          <CenteredCol xs={12} md={6}>
+            <Headline color="black" center>
+              Who We Are
+            </Headline>
+            <p>
+              Good Time Creative is made up of a collection of creatives who
+              have specialized expertise in the event industry. We Thrive on
+              creativity and push the boundaries of experiential development.
+              Our commitment to the excellence and dedication to our client's
+              success is what keeps us up at night.
+            </p>
+          </CenteredCol>
+        </Row>
+
+        <Headline color="black" center>
+          Who We Are
+        </Headline>
+        <TeamList />
+      </Container>
     </StyledSection>
   );
 };
 
 /* STYLED Elements */
-const StyledLogo = styled(Logo)`
-  width: 100%;
-  color: ${theme('colors.white')};
-`;
-
 const CenteredCol = styled(Col)`
   text-align: center;
 `;
 
 const StyledSection = styled(BackgroundImage)`
-  background-color: ${theme('colors.red')};
-  width: 100%;
+  /* background-color: ${theme('colors.red')}; */
+  width: 100vw;
+  min-height: 500px;
   padding: 2rem;
 `;
 
