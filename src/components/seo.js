2e5d91a9da24874bcd608113851c80aa9cd23e46
Keith Hoffmann <hi@keith.work>
2021-02-16 21:27:57 -0600

    feat: add image to props and meta, and address schema

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,11 +10,17 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { useSiteMetadata } from '@hooks/use-site-metadata';
 
-function SEO({ description, lang, meta, title }) {
+function SEO({ description, lang, meta, title, image: metaImage }) {
   const siteMetadata = useSiteMetadata();
 
   const metaDescription = description || siteMetadata.description;
   const defaultTitle = siteMetadata.title;
+  console.log(metaImage);
+
+  const image =
+    metaImage && metaImage.src
+      ? `${siteMetadata.siteUrl}${metaImage.src}`
+      : null;
 
   return (
     <Helmet
@@ -22,11 +28,15 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      titleTemplate={defaultTitle ? `${defaultTitle} | %s` : null}
       meta={[
         {
           name: `description`,
           content: metaDescription,
+        },
+        {
+          name: `keywords`,
+          content: siteMetadata?.keywords || ``,
         },
         {
           property: `og:title`,
@@ -41,10 +51,6 @@ function SEO({ description, lang, meta, title }) {
           content: `website`,
         },
         {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
           name: `twitter:creator`,
           content: siteMetadata?.author || ``,
         },
@@ -56,8 +62,56 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ].concat(meta)}
-    />
+      ]
+        .concat(
+          metaImage
+            ? [
+                {
+                  property: 'og:image',
+                  content: image,
+                },
+                {
+                  property: 'og:image:width',
+                  content: metaImage.width,
+                },
+                {
+                  property: 'og:image:height',
+                  content: metaImage.height,
+                },
+                {
+                  name: 'twitter:card',
+                  content: 'summary_large_image',
+                },
+              ]
+            : [
+                {
+                  name: 'twitter:card',
+                  content: 'summary',
+                },
+              ]
+        )
+        .concat(meta)}
+    >
+      <script type="application/ld+json">
+        {`
+        {
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "url": ${siteMetadata.siteUrl},
+          "name": "${siteMetadata.title}",
+          "email": "${siteMetadata.contactEmail}",
+          "telephone": "${siteMetadata.contactPhone}",
+          "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "${siteMetadata.contactAddressObject.street}",
+            "addressLocality": "${siteMetadata.contactAddressObject.city}",
+            "addressRegion": "${siteMetadata.contactAddressObject.state}",
+            "postalCode": "${siteMetadata.contactAddressObject.postalCode}"
+          },
+        }
+      `}
+      </script>
+    </Helmet>
   );
 }
 
@@ -72,6 +126,11 @@ SEO.propTypes = {
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
+  image: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+  }),
 };
 
 export default SEO;
